b72077054d793e615871df92acf080fb05b18cc4
adiamb <ambati@stanford.edu>
2020-12-01 23:38:46 -0800

    D2

--- scripts/d2.py
+++ scripts/d2.py
@@ -1,17 +1,28 @@
 from collections import defaultdict
 inputD2 = 'inputs/d2_p1.input'
 valid = 0
+valid2 = 0
 with open(inputD2) as inFile:
-    for line in inFile:
-       min_, max_, alph, pwd= line.strip().replace(':', '').replace('-', ' ').split(' ')
-       strDict = defaultdict(int)
-       for i in pwd:
-           strDict[i]+=1
-       if alph in strDict:
-           getOcc = strDict.get(alph)
-           if getOcc >= int(min_) and getOcc <= int(max_):
-               valid += 1
-print('the number of valid passwds {}'.format(valid))
+	for line in inFile:
+		min_, max_, alph, pwd= line.strip().replace(':', '').replace('-', ' ').split(' ')
+		getIndAlpha1 = pwd[int(min_)-1]
+		getIndAlpha2 = pwd[int(max_)-1]
+		strDict = defaultdict(int)
+		for i in pwd:
+			strDict[i]+=1
+		if alph in strDict:
+			getOcc = strDict.get(alph)
+			if getOcc >= int(min_) and getOcc <= int(max_):
+				valid += 1
+		if getIndAlpha1 == alph and getIndAlpha2 != alph:
+			valid2 += 1
+		elif getIndAlpha1 != alph and getIndAlpha2 == alph:
+			valid2 += 1
+			#print(getIndAlpha1, getIndAlpha2, min_, max_, pwd)
+
+
+print('PART1 >>> the number of valid passwds {}'.format(valid))
+print('PART2 >>> the number of valid passwds {}'.format(valid2))
 
 
 
