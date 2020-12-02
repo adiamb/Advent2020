86208228ea3e4f8089cd5087662f2870e3fb37cb
adiamb <ambati@stanford.edu>
2020-12-01 22:58:46 -0800

    D2

--- scripts/d1.py
+++ scripts/d1.py
@@ -3,6 +3,11 @@ d1Input = [int(i.strip()) for i in open('inputs/d1_p1.input')]
 for j in d1Input:
     checkNum = 2020-j
     if checkNum in d1Input:
-        print('The answer is {}'.format(j*checkNum))
-        break
-## Part2
+        print('The answer to part1 is {}'.format(j*checkNum))
+    for i in d1Input: ## Part2
+        checkNum2 = 2020-(j+i)
+        if checkNum2 in d1Input:
+            print('The answer to part2 is {}'.format(j*checkNum2*i))
+            break
+    
+
