9d94270ef903007ed1a2364759a65874d7230d33
adiamb <ambati@stanford.edu>
2020-12-11 08:18:12 -0800

    D9

---
+++ scripts/d9.py
@@ -0,0 +1,22 @@
+            
+def sumSeek(arr, pointer, offset, lag):
+    while pointer < len(arr)-lag:
+        pr  = arr[pointer:offset+1]
+        pointer += 1
+        offset +=1
+        query=pr.pop()
+        querySet = set()
+        for i in pr:
+            if query-i in pr:
+                querySet.add(True)
+        if not querySet:
+            return(query)
+    
+def main():
+    inputFile = 'inputs/d9_p1.input'
+    arr=[int(i.strip()) for i in open(inputFile)]
+    p1=sumSeek(arr, pointer=0, offset=25, lag=25)
+    print('The Answer to part1 is {}'.format(p1))
+    #print('The Answer to part2 is {}'.format(p2))
+
+if __name__ == "__main__":main()
